1fb00adbf4ab822e8f1475afc7c71a3ed85f757c
Kunerkhan <kakerov@gmail.com>
2019-02-05 15:23:22 +0600

    Fixing link - Changed adress

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import { BrowserRouter as Router, Route  } from 'react-router-dom';
 import NewMembersRegistrationPage from "./components/NewMembersRegistrationPage/NewMembersRegistrationPage";
 import Neolabs from "./components/Neolabs/Neolabs";
 
-class App extends Component {
-  render() {
-    return (
-      <Switch>
-        <Route path="/" exact component={MainPage}/>
-        <Route path='/registration' exact component={NewMembersRegistrationPage}/>
-        <Route path="/neolabs" component={Neolabs}/>
-      </Switch>
-    );
-  }
-}
+class App extends React.Component {
+	render() {
+		return (
+			<Switch>
+			<Route exact path="/"  component={MainPage}/>
+			<Route path='/registration' exact component={NewMembersRegistrationPage}/>
+			<Route path='/neolabs' exact component={Neolabs}/>
+			</Switch>
+			);
+		}
+	}
 
-export default App;
+	export default App;
